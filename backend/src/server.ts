9276b989d50beb3b32f70558b690b696b2048673
Antonio Filho <antonioangelo2703@gmail.com>
2026-05-18 22:42:42 -0300

    Adiciona webhook mercado pago

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,6 +6,9 @@ import userRoutes from './routes/user.routes'
 import productRoutes from './routes/product.routes'
 import orderRoutes from './routes/order.routes'
 
+import { prisma } from './lib/prisma'
+import { mercadopago } from './services/mercadoPago'
+
 const app = express()
 
 const PORT = process.env.PORT || 3333
@@ -33,6 +36,55 @@ app.get('/', (req, res) => {
     message: 'API AKsemijoias funcionando'
   })
 })
+
+app.post(
+  '/webhook/mercadopago',
+  async (req, res) => {
+
+    try {
+
+      const paymentId =
+        req.body?.data?.id
+
+      if (!paymentId) {
+        return res.sendStatus(200)
+      }
+
+      const payment =
+        await mercadopago.payment.findById(
+          paymentId
+        )
+
+      const status =
+        payment.body.status
+
+      if (status === 'approved') {
+
+        await prisma.order.updateMany({
+
+          where: {
+            paymentId: paymentId.toString()
+          },
+
+          data: {
+            status: 'PAID'
+          }
+
+        })
+
+      }
+
+      return res.sendStatus(200)
+
+    } catch (error) {
+
+      console.log(error)
+
+      return res.sendStatus(500)
+    }
+
+  }
+)
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`)
