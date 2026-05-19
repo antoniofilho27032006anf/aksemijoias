f5afd71d4c8eae0087c99b12f7b3d5337742d5a3
Antonio Filho <antonioangelo2703@gmail.com>
2026-05-18 21:58:43 -0300

    Corrige api produção

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,11 @@
 import axios from 'axios'
 
+const baseURL =
+  typeof window !== 'undefined' &&
+  window.location.hostname === 'localhost'
+    ? 'http://192.168.1.109:3333'
+    : 'https://aksemijoias-backend.onrender.com'
+
 export const api = axios.create({
-  baseURL: 'http://192.168.1.109:3333'
+  baseURL
 })
